09ff3626d6683e622ef1ef39923e633f4687064a
Najm Hoda <najmkhoda@gmail.com>
2024-10-03 11:10:56 -0700

    Add login/signup links for new users

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,14 @@
 import styles from './Header.module.css'
 import { getAccount } from '@/lib/session';
+import Link from 'next/link';
 
 export default async function Header() {
-    const name = (await getAccount())?.name ?? 'Sign in';
+    const name = (await getAccount())?.name ?? 
+        <>
+            <Link className={styles.userLinks} href='/login'>Log in</Link>
+            <> or </>
+            <Link className={styles.userLinks} href='/signup'>Sign up</Link>
+        </>;
 
     return (
         <header className={styles.container}>
